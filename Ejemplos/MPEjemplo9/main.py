ac068488191324e9ae79335a4dbb3f5fb613f545
ChristianJesusC <jesuschiu505@gmail.com>
2025-02-25 11:21:15 -0600

    Ejemplo 9 terminado

--- Ejemplos/MPEjemplo9/main.py
+++ Ejemplos/MPEjemplo9/main.py
@@ -1,4 +1,6 @@
 from time import sleep
+from multiprocessing import Manager,Process
+from multiprocessing.managers import BaseManager
 
 clientes = []
 
@@ -6,8 +8,32 @@ def monitor():
     print(f'Clientes conectados: {len(clientes)}')
     sleep(2)
     
-def registro(datos[]):
+def registro(datos):
     clientes.append(datos)
     
 def registro_desconexion(datos):
     clientes.pop(datos)
+    
+class myMaganer(BaseManager):
+    pass
+
+if __name__ == '__main__':
+    clientes = Manager().list()
+    myMaganer.register('get_registro', callable=registro)
+    myMaganer.register('get_desconexion', callable=registro_desconexion)
+    
+    manag = myMaganer(address=('localhost', 5000), authkey = b'123')
+    manag.start()
+    
+    p1 = Process(target=monitor, daemon=True)
+    p1.start()
+    
+    try:
+        while True:
+            print("Servidor en servicio")
+            sleep(2)
+    except KeyboardInterrupt:
+        print("Servidor detenido")
+    finally:
+        manag.shutdown()
+        
