f8b172b10b236a6bafc436dfd7487caa52010e84
ChristianJesusC <jesuschiu505@gmail.com>
2025-02-26 08:44:21 -0600

    finalizacion del ejemplo

--- Ejemplos/MPEjemplo9/main.py
+++ Ejemplos/MPEjemplo9/main.py
@@ -1,12 +1,14 @@
 from time import sleep
-from multiprocessing import Manager,Process
+from multiprocessing import Manager,Process, freeze_support
 from multiprocessing.managers import BaseManager
+
 
 clientes = []
 
 def monitor():
-    print(f'Clientes conectados: {len(clientes)}')
-    sleep(2)
+    while True:
+        print("Clientes conectados: ", len(clientes))
+        sleep(2)
     
 def registro(datos):
     clientes.append(datos)
@@ -18,6 +20,7 @@ class myMaganer(BaseManager):
     pass
 
 if __name__ == '__main__':
+    freeze_support()
     clientes = Manager().list()
     myMaganer.register('get_registro', callable=registro)
     myMaganer.register('get_desconexion', callable=registro_desconexion)
